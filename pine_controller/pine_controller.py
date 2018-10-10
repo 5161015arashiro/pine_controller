81824493b02c3140ec1361b38f34967971feb73a
unknown <5161015@sophia.local>
2018-10-10 15:28:47 +0900

    畑指定のテスト

--- pine_controller/pine_controller.py
+++ pine_controller/pine_controller.py
@@ -9,9 +9,9 @@ class pine_controller:
     def message(self, message):
         if message == "ヘルプ":
             self.return_text = "ヘルプはまだない"
-        elif text == "設定確認":
+        elif message == "設定確認":
             self.return_text = "JSONを読んで加工して返そうと思っている"
-        elif text == "畑指定":
+        elif message == "畑指定":
 
             self.return_text = "畑を"
             
