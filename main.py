a4caabf66679b156ff33341f362b3ce581d2e1a2
unknown <5161015@sophia.local>
2018-12-19 15:14:23 +0900

    ターゲットの畑を保存するように変更

--- main.py
+++ main.py
@@ -33,36 +33,37 @@ app = Flask(__name__)
 # @app.route("/callback", methods=['POST'])
 def callback(request):
 # def callback():
-    if request.query.liff is not None:
-        return render_template('timer.html')
-    else:
-        signature = request.headers['X-Line-Signature']
-        # get request body as textvagra
-        body = request.get_data(as_text=True)
+    # if request.query.liff is not None:
+    #     return render_template('timer.html')
+    # else:
+    print(dir(request))
+    signature = request.headers['X-Line-Signature']
+    # get request body as textvagra
+    body = request.get_data(as_text=True)
 
-        # parse webhook body
-        try:
-            events = parser.parse(body, signature)
-        except InvalidSignatureError:
-            exit()
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        exit()
 
-        # if event is MessageEvent and message is TextMessage, then echo text
-        for event in events:
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
 
-            if isinstance(event , PostbackEvent):
-                
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    pine.postback(urllib.parse.parse_qs(event.postback.data))
-                )
+        if isinstance(event , PostbackEvent):
             
-            if isinstance(event, MessageEvent) and isinstance(event.message, TextMessage):
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    pine.reply_message(event.message.text)
-                )
+            line_bot_api.reply_message(
+                event.reply_token,
+                pine.postback(urllib.parse.parse_qs(event.postback.data))
+            )
+        
+        if isinstance(event, MessageEvent) and isinstance(event.message, TextMessage):
+            line_bot_api.reply_message(
+                event.reply_token,
+                pine.reply_message(event.message.text)
+            )
 
-        return 'OK'
+    return 'OK'
 
 # # @app.route("/liff", methods=['GET'])
 # def liff():
